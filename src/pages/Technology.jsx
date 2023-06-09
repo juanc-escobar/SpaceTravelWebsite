fa1eb0f09d6dfe49b2a4b5932369848e12b86945
juanc-escobar <juanc-escobar@hotmail.com>
2023-06-08 22:59:25 -0500

    styling the technology page, still need to fix ultra wide and medias

--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -15,16 +15,19 @@ import data from "../data.json"
     }
 
     return (
-      <main id="main" className="grid-container grid-container--crew flow">
+      <main id="main" className="grid-container grid-container--technology flow">
           <h1 className="numbered-title uppercase"><span aria-hidden="true" >03</span>Space launch 101</h1>
           <picture>
-          <img src={images.landscape} alt={name} />
+            <source srcSet={images.portrait} media="(min-width: 45em)" className="full-width" />
+          <img src={images.landscape} alt={name} className="full-width" />
           </picture>
           <NavNumbers selectTech={selectTech} handleTech={handleTech} />
-          <article className="crew-details flow">
-              <p className="subtitle">The terminology...</p>
-              <h2 className="fs-600 ff-serif uppercase">{name}</h2>
-              <p>{description}</p>
+          <article className="technology-details flow">
+              <div className="tech-title flow">
+                <p className="subtitle fs-400  --ff-sans-cond uppercase text-light">The terminology...</p>
+                <h2 className="fs-700 ff-serif uppercase">{name}</h2>
+              </div>
+              <p className="fs-400 --ff-sans-normal text-light">{description}</p>
           </article>
     </main>
     )
